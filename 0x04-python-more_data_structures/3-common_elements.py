7090d24b05e952cabf196029232acd097244d984
lazesla <eslalazarus23@gmail.com>
2023-03-15 08:47:55 -0700

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,4 +3,4 @@ def common_elements(set_1, set_2):
 	set_1 = {"Python", "C", "Javascript"}
 	set_2 = {"Bash", "C", "Ruby", "Perl" }
 	c_set = common_elements(set_1, set_2)
-	print(sort(list(c_set)))
+        return set_1 & set_2
