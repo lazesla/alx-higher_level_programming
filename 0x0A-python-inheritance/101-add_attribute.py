411b032393308d774838bbc85c3075d7bfa75662
Esla Lazarus <eslalazarus23@gmail.com>
2023-04-11 20:10:13 -0700

    task 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,26 +1,12 @@
-!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
+#!/usr/bin/python3
+"""Module containing add_attribute method"""
 
 
-class Square(Rectangle):
-    """Square class that inherits from Rectangle that inherits BaseGeometry"""
-
-    def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
-        super().__init__(size, size)
-        self.integer_validator("size", size)
-        self.__size = size
-
-    def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
-
-    def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
+def add_attribute(obj, name, value):
+    """Method checking if attribute can be set and sets
+    where possible"""
+    if hasattr(obj, "__dict__") or \
+       (hasattr(obj, "__slots__") and name in obj.__slots__):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
