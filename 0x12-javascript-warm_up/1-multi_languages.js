2341c341c29cb4c60f8d585a22c865025feef74f
Esla Lazarus <eslalazarus23@gmail.com>
2023-06-13 14:51:51 -0700

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+// Prints 3 lines
+
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'Javascript is amazing';
+
+console.log(firstLine + '\n' + secondLine + '\n' +
+                thirdLine);
