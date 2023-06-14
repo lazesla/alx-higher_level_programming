7bcd20c43d8454d22ce825b96ebc0f0e9361b3bd
Esla Lazarus <eslalazarus23@gmail.com>
2023-06-14 13:39:50 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-for (const line of['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+// Uses an array to loop through and print strings
+
+const langs = ['C is fun'"\n" 'Python is cool'"\n"'Javascript is amazing'];
+for (let i = 0; i < langs.length; i++) {
+  console.log(langs[i]);
 }
