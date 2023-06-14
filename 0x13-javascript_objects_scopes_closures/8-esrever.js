29f8a0454f38a4cebbe51d54e4976abfd5c9be8d
Esla Lazarus <eslalazarus23@gmail.com>
2023-06-14 08:22:09 -0700

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
-// Reverses a list
-
+// reverses a list
 exports.esrever = function (list) {
-  let left = 0;
-  let right = list.length - 1;
-  while (left <= right) {
-    let temp = list[right];
-    list[right] = list[left];
-    list[left] = temp;
-    left++;
-    right--;
-  }
-  return (list);
+  return list.sort(() => 1);
 };
